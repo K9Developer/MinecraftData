99b1bc752dee70873b5afd3a99d039be437ca1a8
Ilai <66069146+K9Developer@users.noreply.github.com>
2024-09-11 22:47:45 +0300

    Update main.py

--- item_fetching/main.py
+++ item_fetching/main.py
@@ -203,6 +203,7 @@ def main():
     with open("../items/atlas_metadata.json", "w") as f:
         json.dump(metadata, f, indent=4)
 
+    print("[i] The CWD is", os.path.dirname(os.path.realpath(__file__)))
     print("✅ Minecraft Atlas Generator completed successfully!")
 
 
